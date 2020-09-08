46ee7fc391ac1417eb1c6b416fcbfb46cd9189fb
Sílvio Gatis Filho <silviogatisf@gmail.com>
2020-09-08 16:58:00 -0300

    organização do código pt1
    organização do cod

--- main.py
+++ main.py
@@ -20,7 +20,6 @@ class Window(QMainWindow):
         self._createCentralWidget()
         self.setGeometry(300, 300, 750, 600)
 
-
     def _createMenu(self):
         """"Main menu"""
         self.menu = self.menuBar().addMenu("&Menu")
@@ -37,25 +36,17 @@ class Window(QMainWindow):
         self.tools.setFixedHeight(36)
         self.tools.setStyleSheet("QToolBar{spacing:5px;}")
 
-#        folderOpen = QtWidgets.QAction(QtGui.QIcon("add_pack.ico"), "Open Folder", tools)   #Dunno how to use it... but shows an icon instead of text, Might be useful
-#        tools.addAction(folderOpen)
-
         # BTN Add char
         addCharBtn = QPushButton()
         addCharBtn.setIcon(QIcon('addchar.png'))
         self.tools.addWidget(addCharBtn)
-        addCharBtn.clicked.connect(self.addChar)
-
-        # BTN Monster
-        addMonBtn = QPushButton()
-        addMonBtn.setIcon(QIcon('monster.png'))
-        self.tools.addWidget(addMonBtn)
+        addCharBtn.clicked.connect(self._addChar)
 
         #BTN Remove char
         removeCharBtn = QPushButton()
         removeCharBtn.setIcon(QIcon('removechar.png'))
         self.tools.addWidget(removeCharBtn)
-        removeCharBtn.clicked.connect(self.removeChar)
+        removeCharBtn.clicked.connect(self._removeChar)
 
         # BTN Sort
         sortBtn = QPushButton()
@@ -94,12 +85,9 @@ class Window(QMainWindow):
         self.btnApplyEffect = QPushButton('Apply')
         self.tools.addWidget(self.btnApplyEffect)
 
-    # Upadate comboVal function
+    # Upadate comboVal method
     def _updateCondVal(self):
         condition = self.condCombo.currentText()
-        # ordinaryCombo1 = QComboBox
-        # ordinaryCombo2 = QComboBox
-        # ordinaryCombo3 = QComboBox
         if condition in conditionGeneral:
             self.comboVal.clear()
             for val1 in conditionGeneralValue:
@@ -113,76 +101,88 @@ class Window(QMainWindow):
             for val3 in persistentDamageValue:
                 self.comboVal.addItem(val3)
 
-    def addChar(self):
+    # addchar method
+    def _addChar(self):
         self.charCount = self.charCount + 1
-        self.populateCharWidget(True)
+        self._populateCharWidget(True)
 
-    def removeChar(self):
+    # removeChar method
+    def _removeChar(self):
         if self.charCount > 1:
             self.charCount = self.charCount - 1
-            self.populateCharWidget(False)
+            self._populateCharWidget(False)
         else:
             pass
 
+
+    # MainWindow CentralWidget definition
     def _createCentralWidget(self):
         """Central Widget"""
-        layout = QVBoxLayout()
+        centralWid = QWidget()
 
-        scroll = QScrollArea()          # Scroll Area which contains the widgets
-        widget = QWidget()              # Widget that contains the collection of Vertical Box
-        self.vbox = QVBoxLayout()            # The Vertical Box that contains _charLayout
-
-        self.vbox.setObjectName("vbox")
+        # Scroll area and wrap widget definition
+        charScroll = QScrollArea()          # Scroll Area which contains the wrapWidget and charWidgets
+        charWrapWidget = QWidget()              # Widget that contains the collection of Vertical Box
+        # Char widget vertical layout box
+        self.charBox = QVBoxLayout()            # The Vertical Box that contains _charLayout
+        self.charBox.setObjectName('charBox')
         self.charCount = 1
         self.charList = []
 
-
+        # Central layout of the Central widget
+        layout = QVBoxLayout()
         layout.addWidget(self._roundWidget())           #Calling RoundInfo widget
-        layout.addWidget(scroll)
+        layout.addWidget(charScroll)
 
         for i in range(self.charCount):
-            self.populateCharWidget(True)
+            self._populateCharWidget(True)
 
         """Scroll Area Properties"""
-        widget.setLayout(self.vbox)
-        scroll.setWidgetResizable(True)
-        scroll.setWidget(widget)
+        charWrapWidget.setLayout(self.charBox)
+        charWrapWidget.setLayout(self.charBox)
+        charScroll.setWidgetResizable(True)
+        charScroll.setWidget(charWrapWidget)
 
-        centraWid = QWidget()
-        centraWid.setLayout(layout)
-        self.setCentralWidget(centraWid)              #Changed from QMdiArea to Qwidget, still confused on how to use it
+        # centralWidget layout definition
+        centralWid.setLayout(layout)
+        self.setCentralWidget(centralWid)
 
-    def populateCharWidget(self, signal):
+    # PopulateCharWidget Method
+    def _populateCharWidget(self, signal):
 
         if signal:
             self.charList.append(len(self.charList))
             per = self.charList[-1]
             per = personagem()
-            self.vbox.addWidget(per.createWidget(self.charList[-1]))  # Calling character widget
-            print(self.vbox.count())
+            self.charBox.addWidget(per.createWidget(self.charList[-1]))  # Calling character widget
+            print(self.charBox.count())
         elif not signal:
-            self.vbox.itemAt(self.vbox.count()-1).widget().setParent(None)
+            self.charBox.itemAt(self.charBox.count()-1).widget().setParent(None)
 
-
+    # RoundWidget definition
     def _roundWidget(self):
         """Top Widget: Informations before characters, called on _createCentralWidget"""
 
-        # creates the round counter
-        roundNumber = 0
-        roundDisplay = QLabel(f"<h1>ROUND: {roundNumber}</h1>") # aqui eu tou usando f-string, é uma coisa do python3 pra colocar jogar a variável dentro da string de uma maneira mais fácil.
-        # layout of the first line of roundDisplay
+        # DEVE RECEBER O TURNO DO MOMENTO (QUANDO A GENTE FOR FAZER A ROTINA O PERSONAGEM DO MOMENTO VAI SER SELECIONADO E OS DADOS DELE VÃO APARECER AQUI)
+        # currentTurn = turno()
+
+        # Round counter interface
+        roundNumber = 0 # VAI PUXAR DA CLASSE RODADA QUE VAI ITERAR TODA VEZ QUE O TURNO DE TODOS OS PERSONAGENS FOREM IGUAL A RODADA + 1
+        roundDisplay = QLabel(f"<h1>ROUND: {roundNumber}</h1>")
+        # First line of roundDisplay layout
         roundDisplayLayout1 = QHBoxLayout()
-        # roundDisplay.setLayout(roundDisplayLayout1)
         roundDisplayLayout1.addWidget(roundDisplay)
-        roundDisplayLayout1.addStretch(1) # isso daqui é um espaçador para empurrar o label pra esquerda
+        roundDisplayLayout1.addStretch(1)
 
         # creates the line with the char's turn, actions e buttons to advance and return an turn / slash round
-        turnDisplay = QLabel("<h2>Character's turn<h2>") # essa parada ta de placeholder, a gente tem que colocar o personagem a partir da função que determina o turno do personagem.
-        turnDisplay.setFixedHeight(20)
-        turnDisplayLayout = QVBoxLayout()
-        turnDisplayLayout.addStretch(10)
-        turnDisplayLayout.addWidget(turnDisplay)
-        turnDisplayLayout.addStretch(10)
+        # char's turn display
+        charTurnDisplay = QLabel("<h2>Character's turn<h2>") # VAI PUXAR DO JOGADOR QUE ESTIVER COM O ATRIBUTO TURN = TRUE
+       # improvised solution to align the label with the rest of the layout
+        charTurnDisplay.setFixedHeight(20)
+        charTurnDisplayLayout = QVBoxLayout()
+        charTurnDisplayLayout.addStretch(10)
+        charTurnDisplayLayout.addWidget(charTurnDisplay)
+        charTurnDisplayLayout.addStretch(10)
 
         # previous turn button
         previousTurn = QPushButton(self)
@@ -190,8 +190,8 @@ class Window(QMainWindow):
         previousTurn.setIcon(QIcon('circle-left.png'))
         previousTurn.setIconSize(QSize(24,24))
 
-        # actions logic
-        actionsNumber = 3 # por default o jogador deve ter 3 ações, as condições do personagem vão interferir nisso.
+        # actions icons
+        actionsNumber = 3 # TAMBÉM VAI PUXAR DO JOGADOR QUE ESTIVER COM O ATRIBUTO TURN = TRUE
         # actions layout
         actionLayout = QHBoxLayout()
         for action in range(0, actionsNumber):
@@ -209,37 +209,33 @@ class Window(QMainWindow):
 
         # layout of the second line of the roundDysplay
         roundDisplayLayout2 = QHBoxLayout()
-        roundDisplayLayout2.addLayout(turnDisplayLayout)
+        roundDisplayLayout2.addLayout(charTurnDisplayLayout)
         roundDisplayLayout2.addStretch(1)
         roundDisplayLayout2.addWidget(previousTurn)
         roundDisplayLayout2.addSpacing(10)
         roundDisplayLayout2.addLayout(actionLayout)
         roundDisplayLayout2.addWidget(nextTurn)
 
-        # toplayout of roundisplay
-        self.toplayoutWidget = QWidget()
-        self.toplayoutWidget.setFixedHeight(85)
-        self.toplayout = QVBoxLayout()
-        self.toplayoutWidget.setLayout(self.toplayout)
-        self.toplayout.addLayout(roundDisplayLayout1)
-        self.toplayout.addLayout(roundDisplayLayout2)
+        # roundDisplay widget definition
+        self.roundTopLayoutWidget = QWidget()
+        self.roundTopLayoutWidget.setFixedHeight(85)
+        # roundDisplayLayoutTop
+        self.roundTopLayout = QVBoxLayout()
+        self.roundTopLayoutWidget.setLayout(self.roundTopLayout)
+        self.roundTopLayout.addLayout(roundDisplayLayout1)
+        self.roundTopLayout.addLayout(roundDisplayLayout2)
 
-        self.setLayout(self.toplayout)
-        return self.toplayoutWidget
+        self.setLayout(self.roundTopLayout)
+        return self.roundTopLayoutWidget
 
+    # charWidget definition
+    # def _charWidget(self):
+
+    # statusBar definition
     def _createStatusBar(self):
         status = QStatusBar()
         status.showMessage("Let's roll the dice")
         self.setStatusBar(status)
-
-# ConditionData
-conditionList = ['NA', 'Blinded', 'Broken(object)', 'Clumsy', 'Concealed','Confused', 'Controled', 'Dazzled', 'Deafened', 'Doomed','Drained', 'Dying', 'Encumbered', 'Enfleebed', 'Fascinated', 'Fatigued', 'Flat-Footed', 'Fleeing', 'Friendly', 'Frightened', 'Grabbed', 'Helpful', 'Hidden', 'Hostile', 'Immobilized', 'Indifferent', 'Invisible', 'Observed', 'Persistent Damage', 'Petrified', 'Prone', 'Quickened', 'Restrained', 'Sickened', 'Slowed', 'Stunned', 'Stupefied', 'Unconscious', 'Undetected', 'Unfriendly', 'Unnoticed', 'Wounded']
-conditionGeneral = ['NA', 'Blinded', 'Broken(object)', 'Concealed','Confused', 'Controled', 'Dazzled', 'Deafened', 'Encumbered', 'Fascinated', 'Fatigued', 'Flat-Footed', 'Fleeing', 'Friendly', 'Grabbed', 'Helpful', 'Hidden', 'Hostile', 'Immobilized', 'Indifferent', 'Invisible', 'Observed', 'Petrified', 'Prone', 'Quickened', 'Restrained', 'Unconscious', 'Undetected', 'Unfriendly', 'Unnoticed']
-conditionCount = ['Clumsy', 'Doomed','Drained', 'Dying', 'Enfleebed', 'Frightened', 'Sickened', 'Slowed', 'Stunned', 'Stupefied', 'Wounded']
-# valueData
-conditionGeneralValue = ['NA', '1 round', '2 rounds', '3 rounds', '4 rounds', '5 rounds', '6 rounds', '10 rounds', '20 rounds', 'other']
-conditionCountValue = ['NA', '1', '2', '3', '4', '5']
-persistentDamageValue = ['NA', '1d4', '2d4', '3d4', '1d6', '2d6', '3d6', '1d8', '2d8', 'other']
 
 class personagem(QWidget):
     def __init__(self, parent = None):
@@ -374,7 +370,84 @@ class personagem(QWidget):
 
         return charlayoytWidget
 
+#roundWidget class
+# class roundWidget(QWidget):
+#     def __init__(self, roundWidget, parent = None): # TAMBÉM DEVE ENTRAR O PARÂMETRO ROUND AQUI, QUE DEVE SER A CHAMADA DA DE ROUND()
+#         super(roundWidget, self).__init__(parent)
+#         self.roundWidget = roundWidget
+#         #self.round = round()
+#
+#     # RoundWidget definition
+#     def _roundWidget(self):
+#         """Top Widget: Informations before characters, called on _createCentralWidget"""
+#
+#         # DEVE RECEBER O TURNO DO MOMENTO (QUANDO A GENTE FOR FAZER A ROTINA O PERSONAGEM DO MOMENTO VAI SER SELECIONADO E OS DADOS DELE VÃO APARECER AQUI)
+#         # currentTurn = turno()
+#
+#         # Round counter interface
+#         roundNumber = 0 # VAI PUXAR DA CLASSE RODADA QUE VAI ITERAR TODA VEZ QUE O TURNO DE TODOS OS PERSONAGENS FOREM IGUAL A RODADA + 1
+#         roundDisplay = QLabel(f"<h1>ROUND: {roundNumber}</h1>")
+#         # First line of roundDisplay layout
+#         roundDisplayLayout1 = QHBoxLayout()
+#         roundDisplayLayout1.addWidget(roundDisplay)
+#         roundDisplayLayout1.addStretch(1)
+#
+#         # creates the line with the char's turn, actions e buttons to advance and return an turn / slash round
+#         # char's turn display
+#         charTurnDisplay = QLabel("<h2>Character's turn<h2>") # VAI PUXAR DO JOGADOR QUE ESTIVER COM O ATRIBUTO TURN = TRUE
+#        # improvised solution to align the label with the rest of the layout
+#         charTurnDisplay.setFixedHeight(20)
+#         charTurnDisplayLayout = QVBoxLayout()
+#         charTurnDisplayLayout.addStretch(10)
+#         charTurnDisplayLayout.addWidget(charTurnDisplay)
+#         charTurnDisplayLayout.addStretch(10)
+#
+#         # previous turn button
+#         previousTurn = QPushButton(self)
+#         previousTurn.resize(24,24)
+#         previousTurn.setIcon(QIcon('circle-left.png'))
+#         previousTurn.setIconSize(QSize(24,24))
+#
+#         # actions icons
+#         actionsNumber = 3 # TAMBÉM VAI PUXAR DO JOGADOR QUE ESTIVER COM O ATRIBUTO TURN = TRUE
+#         # actions layout
+#         actionLayout = QHBoxLayout()
+#         for action in range(0, actionsNumber):
+#             actionsIcon = QLabel(self)
+#             actionsIconPixmap = QPixmap('action.png')
+#             actionsIcon.setPixmap(actionsIconPixmap)
+#             actionLayout.addWidget(actionsIcon)
+#             actionLayout.addSpacing(10)
+#
+#         # next turn button
+#         nextTurn = QPushButton(self)
+#         nextTurn.resize(24,24)
+#         nextTurn.setIcon(QIcon('circle-right.png'))
+#         nextTurn.setIconSize(QSize(24,24))
+#
+#         # layout of the second line of the roundDysplay
+#         roundDisplayLayout2 = QHBoxLayout()
+#         roundDisplayLayout2.addLayout(charTurnDisplayLayout)
+#         roundDisplayLayout2.addStretch(1)
+#         roundDisplayLayout2.addWidget(previousTurn)
+#         roundDisplayLayout2.addSpacing(10)
+#         roundDisplayLayout2.addLayout(actionLayout)
+#         roundDisplayLayout2.addWidget(nextTurn)
+#
+#         # roundDisplay widget definition
+#         self.roundTopLayoutWidget = QWidget()
+#         self.roundTopLayoutWidget.setFixedHeight(85)
+#         # roundDisplayLayoutTop
+#         self.roundTopLayout = QVBoxLayout()
+#         self.roundTopLayoutWidget.setLayout(self.roundTopLayout)
+#         self.roundTopLayout.addLayout(roundDisplayLayout1)
+#         self.roundTopLayout.addLayout(roundDisplayLayout2)
+#
+#         self.setLayout(self.roundTopLayout)
+#         return self.roundTopLayoutWidget
+
 # Make a custom label widget (mostly for its mousePressEvent) Used to hide characters name
+
 class BuddyLabel(QLabel):
     def __init__(self, buddy, parent = None):
         super(BuddyLabel, self).__init__(parent)
@@ -386,6 +459,14 @@ class BuddyLabel(QLabel):
         self.buddy.show()
         self.buddy.setFocus() # Set focus on buddy so user doesn't have to click again
 
+# ConditionData
+conditionList = ['NA', 'Blinded', 'Broken(object)', 'Clumsy', 'Concealed','Confused', 'Controled', 'Dazzled', 'Deafened', 'Doomed','Drained', 'Dying', 'Encumbered', 'Enfleebed', 'Fascinated', 'Fatigued', 'Flat-Footed', 'Fleeing', 'Friendly', 'Frightened', 'Grabbed', 'Helpful', 'Hidden', 'Hostile', 'Immobilized', 'Indifferent', 'Invisible', 'Observed', 'Persistent Damage', 'Petrified', 'Prone', 'Quickened', 'Restrained', 'Sickened', 'Slowed', 'Stunned', 'Stupefied', 'Unconscious', 'Undetected', 'Unfriendly', 'Unnoticed', 'Wounded']
+conditionGeneral = ['NA', 'Blinded', 'Broken(object)', 'Concealed','Confused', 'Controled', 'Dazzled', 'Deafened', 'Encumbered', 'Fascinated', 'Fatigued', 'Flat-Footed', 'Fleeing', 'Friendly', 'Grabbed', 'Helpful', 'Hidden', 'Hostile', 'Immobilized', 'Indifferent', 'Invisible', 'Observed', 'Petrified', 'Prone', 'Quickened', 'Restrained', 'Unconscious', 'Undetected', 'Unfriendly', 'Unnoticed']
+conditionCount = ['Clumsy', 'Doomed','Drained', 'Dying', 'Enfleebed', 'Frightened', 'Sickened', 'Slowed', 'Stunned', 'Stupefied', 'Wounded']
+# valueData
+conditionGeneralValue = ['NA', '1 round', '2 rounds', '3 rounds', '4 rounds', '5 rounds', '6 rounds', '10 rounds', '20 rounds', 'other']
+conditionCountValue = ['NA', '1', '2', '3', '4', '5']
+persistentDamageValue = ['NA', '1d4', '2d4', '3d4', '1d6', '2d6', '3d6', '1d8', '2d8', 'other']
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
